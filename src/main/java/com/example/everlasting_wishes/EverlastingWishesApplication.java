2353e0932741cca8ae3b05f9b8b4c3f7d012a4d5
Богдан Сава <bogdansava@MacBook-Pro-Bogdan.local>
2021-04-10 18:24:58 +0300

    create new project

---
+++ src/main/java/com/example/everlasting_wishes/EverlastingWishesApplication.java
@@ -0,0 +1,13 @@
+package com.example.everlasting_wishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverlastingWishesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverlastingWishesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/everlasting_wishes/EverlastingWishesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.everlasting_wishes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverlastingWishesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
